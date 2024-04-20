b57ffd90393d29d37cabecc886f95f76e740fb9f
Qf.Ding <Qf.Ding@yh>
2024-04-20 15:37:29 +0800

    feature

--- sort/src/main/java/com/philding/Topic02_27.java
+++ sort/src/main/java/com/philding/Topic02_27.java
@@ -1,7 +1,15 @@
 package com.philding;
 
 public class Topic02_27 {
+
     public static void main(String[] args) {
 
     }
+
+
+    public static int removeElement(int[] nums, int val) {
+
+        return 0;
+    }
+
 }
