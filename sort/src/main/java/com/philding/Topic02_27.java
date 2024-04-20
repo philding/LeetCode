f4610a5ea18fc47670bebde609280447bae2c5e9
Qf.Ding <Qf.Ding@yh>
2024-04-20 16:13:23 +0800

    feature

--- sort/src/main/java/com/philding/Topic02_27.java
+++ sort/src/main/java/com/philding/Topic02_27.java
@@ -1,15 +1,33 @@
 package com.philding;
 
+import java.util.Arrays;
+
 public class Topic02_27 {
 
     public static void main(String[] args) {
+        int[] arr = {3, 2, 2, 3};
+        int var = 3;
+        int i = removeElement(arr, var);
+
+
+        System.out.printf(String.valueOf(i));
+        System.out.println(Arrays.toString(arr));
+
 
     }
 
 
     public static int removeElement(int[] nums, int val) {
-
-        return 0;
+        int length = nums.length;
+        int left = 0;
+        int right = 0;
+        while (right < length) {
+            if (nums[right] != val) {
+                nums[left++] = nums[right];
+            }
+            right++;
+        }
+        return left;
     }
 
 }
