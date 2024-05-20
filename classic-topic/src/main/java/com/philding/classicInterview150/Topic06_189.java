47c1c0c8fa73f1c69e54969f90f045cc6d745065
Qf.Ding <Qf.Ding@yh>
2024-05-20 15:15:41 +0800

    test

---
+++ classic-topic/src/main/java/com/philding/classicInterview150/Topic06_189.java
@@ -0,0 +1,19 @@
+package com.philding.classicInterview150;
+
+/**
+ * 给定一个整数数组 nums，将数组中的元素向右轮转 k 个位置，其中 k 是非负数。
+ */
+
+public class Topic06_189 {
+    public static void main(String[] args) {
+
+
+
+    }
+
+
+    public static void rotate(int[] nums, int k) {
+
+    }
+
+}
