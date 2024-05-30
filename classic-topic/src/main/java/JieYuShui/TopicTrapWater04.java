66791f29519088fb47d2cd3be0c380dc501a2309
Qf.Ding <Qf.Ding@yh>
2024-05-30 16:07:07 +0800

    feature

--- classic-topic/src/main/java/JieYuShui/TopicTrapWater04.java
+++ classic-topic/src/main/java/JieYuShui/TopicTrapWater04.java
@@ -35,21 +35,21 @@ public class TopicTrapWater04 {
      * @return
      */
     public static int trap(int[] height) {
-        int ans = 0;
+        int trapWater = 0;
         int left = 0, right = height.length - 1;
         int leftMax = 0, rightMax = 0;
         while (left < right) {
             leftMax = Math.max(leftMax, height[left]);
             rightMax = Math.max(rightMax, height[right]);
-            if (height[left] < height[right]) {
-                ans += leftMax - height[left];
-                ++left;
+            if (leftMax < rightMax) {
+                trapWater += leftMax - height[left];
+                left++;
             } else {
-                ans += rightMax - height[right];
-                --right;
+                trapWater += rightMax - height[right];
+                right--;
             }
         }
-        return ans;
+        return trapWater;
 
     }
 
