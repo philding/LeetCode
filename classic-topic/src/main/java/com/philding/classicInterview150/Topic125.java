c9883f1e7aa5e15459d9f62f486297b525f9a52a
Qf.Ding <Qf.Ding@yh>
2024-05-31 10:16:27 +0800

    feature

---
+++ classic-topic/src/main/java/com/philding/classicInterview150/Topic125.java
@@ -0,0 +1,15 @@
+package com.philding.classicInterview150;
+
+public class Topic125 {
+    public static void main(String[] args) {
+
+
+    }
+
+
+    public static boolean isPalindrome(String s) {
+
+        return true;
+    }
+
+}
